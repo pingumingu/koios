214f2127cdd3be1d4db4e6caa13dacd6f59254ac
pingumingu <68141634+pingumingu@users.noreply.github.com>
2022-04-17 16:10:14 +1000

    Added the quadratic equation python file and also added function pointers to the ProblemInstance class so that there can be a different function for each type of problem

---
+++ problems/quadratic_equation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def gen_problem(variables):
+    variables = variables.split(',')
+    a = int(variables[0])
+    b = int(variables[1])
+    c = int(variables[2])
+    problem = f'{a}x^2 + {b}x + {c}=0'
+    solution_1 = (-b + np.sqrt(b**2 - 4*a*c))/(2*a)
+    solution_2 = (-b - np.sqrt(b**2 - 4*a*c))/(2*a)
+    return {'problem': problem,  'solution' : f'x = {str(solution_1)}, x = {str(solution_2)}', 'worked_solution' : f'x = {str(solution_1)}, x = {str(solution_2)}'}
+
--- quadratics_trainer/models.py
+++ quadratics_trainer/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
+import importlib
 # Create your models here.
+#perhaps write some logic here for the generated solutions
+# for the quadratics trainer? can use a folder later with separate python files for each type of problem
+#doesn't have to be in a class, can just be some function definitions, can put in a separate file later
+
 
 class Tag(models.Model):
     """Model representing a book genre."""
@@ -17,11 +22,9 @@ class Problem(models.Model):
     name = models.CharField(max_length = 50, help_text = 'Enter the name of the base problem (e.g. Quadratic Equation)')
 
     tag = models.ManyToManyField(Tag, help_text = 'Select tags for this problem')
-
-    problem_text = models.TextField(max_length = 2000,
-        help_text = 'Enter the text for the problem, with the variables separated by spaces named !var!')
-    solution_text = models.TextField(max_length = 10000,
-        help_text = 'Enter the text for the worked solution, with the variables separated by spaces named !var!')
+    
+    problem_module = models.CharField(max_length = 100)
+    problem_function = models.CharField(max_length = 100)
 
     def __str__(self):
         """
@@ -33,29 +36,6 @@ class Problem(models.Model):
         """Returns the URL to access a detail record for this Problem."""
         return reverse('problem-detail', args=[str(self.id)])
 
-#class ProblemSolutionText(models.Model):
-#    """
-#    Model representing the problem and solution text of a 'base problem'
-#    """
-#    #if A has a B, then B should have the one-to-one field
-#    problem = models.OneToOneField(Problem, help_text = 'Select the problem for this problem/solution text set')
-#
-#    problem_text = models.TextField(max_length = 2000,
-#        help_text = 'Enter the text for the problem, with the variables separated by spaces named !var!')
-#    solution_text = models.TextField(max_length = 10000,
-#        help_text = 'Enter the text for the worked solution, with the variables separated by spaces named !var!')
-#
-#    def __str__(self):
-#        """
-#        String for representing the Model object.
-#        """
-#        return f'problem/solution text of {self.problem.name}'
-#
-#    def get_fstring(self):
-#        """
-#        Return the """
-
-    
 import uuid # Required for unique problem instances
  
 class ProblemInstance(models.Model):
@@ -67,16 +47,11 @@ class ProblemInstance(models.Model):
     #null=True allows the database to stoer a Null value if no base Problem is selected, we don't want this
     #on_delete=models.PROTECT prevents the base problem from being deleted as long as it is referenced by a Problem Instance.
     problem = models.ForeignKey(Problem, on_delete=models.PROTECT, null=False)
-
     variables = models.CharField(max_length = 100, help_text = 'Enter the variables separated by commas')
 
-    def get_problem_text_instance(self):
-        problem_text_instance = self.problem.problem_text
-        problem_text_instance_list = problem_text_instance.split('!var!')
-        variable_list = self.variables.split(',')
-        return ''.join([i + j for i,j in zip(problem_text_instance_list, variable_list)])
+    def get_problem_solution_data(self):
+        module_name = self.problem.problem_module
+        function_name = self.problem.problem_function
 
-    def get_solution_text_instance():
-
-
-    
+        module = importlib.import_module(module_name)
+        return getattr(module, function_name)(self.variables)
